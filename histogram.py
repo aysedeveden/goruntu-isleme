65363c9b9c7168882a23c0374149be6c694bee08
aysedeveden <116079495+aysedeveden@users.noreply.github.com>
2022-11-01 18:40:10 +0300

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -7,23 +7,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-
 resim= cv2.imread("baboon.jpg",0)
-cv2.imshow("Resim",resim)
-  
-
-
-
+cv2.imshow("Resim",resim)  
 
 plt.hist(resim.ravel(),256,[0,256])
-
-
 plt.show()
 
-
-print(resim)             #matrisleri gösterir
+print(resim)            
 print (resim.shape)  
 
 cv2.waitKey(0)           
